5bbf474215820286ffe697e0138a2266087ebca1
Aminulhoque01 <aminuhoque53@gmail.com>
2026-09-05 08:00:58 +0600

    project artichitecture file management

---
+++ redux/features/api/baseApi.ts
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "api",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+
+      headers.set("Content-Type", "application/json");
+
+      return headers;
+    },
+  }),
+
+  tagTypes: [
+    "Auth",
+    "Board",
+    "Column",
+    "Task",
+    "User",
+  ],
+
+  endpoints: () => ({}),
+});
